50497167a113389c0d5f6f6207911f22f6d7c0d6
RaviChauhan <ravic@o365.siprtc.io>
2024-04-04 12:02:33 +0530

    init commit

---
+++ server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "${method} ${uri}\n",
+	}))
+
+	// Routes
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
+}
